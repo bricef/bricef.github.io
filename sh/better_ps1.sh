696e5bf0b614489992f341fcb37afb05a3c3fb17
Brice Fernandes <brice@weave.works>
2018-02-23 12:17:48 +0000

    Add script for curling

---
+++ sh/better_ps1.sh
@@ -0,0 +1,23 @@
+B_BLACK="\[\e[40m\]"
+B_RED="\[\e[41m\]"
+B_GREEN="\[\e[42m\]"
+B_YELLOW="\[\e[43m\]"
+B_BLUE="\[\e[44m\]"
+B_MAGENTA="\[\e[45m\]"
+B_CYAN="\[\e[46m\]"
+B_WHITE="\[\e[47m\]"
+
+F_BLACK="\[\e[30m\]"
+F_RED="\[\e[31m\]"
+F_GREEN="\[\e[32m\]"
+F_YELLOW="\[\e[33m\]"
+F_BLUE="\[\e[34m\]"
+F_MAGENTA="\[\e[35m\]"
+F_CYAN="\[\e[36m\]"
+F_WHITE="\[\e[37m\]"
+
+BOLD="\[\e[1m\]"
+
+END="\[\e[0m\]"
+
+export PS1=$BOLD$F_YELLOW$PS1$END
